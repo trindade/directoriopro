294bf9fcce568dae51df8538152b7427d8dff734
Miquel Camps Orteza <gafeman@gmail.com>
2012-06-01 21:03:34 +0200

    fix html forum thread

--- src/Application/ForumBundle/Controller/ThreadController.php
+++ src/Application/ForumBundle/Controller/ThreadController.php
@@ -109,7 +109,7 @@ class ThreadController extends Controller
 
             // limpiar html
             $entity->setTitle( strip_tags( $entity->getTitle() ) );
-            $entity->setBody( strip_tags( $entity->getBody() ) );
+            $entity->setBody( htmlentities( $entity->getBody() ) );
 
             $slug = $entity->getTitle();
             $entity->setSlug(Util::slugify($slug));
@@ -207,7 +207,7 @@ class ThreadController extends Controller
 
                 // limpiar html
                 $entity->setTitle( strip_tags( $entity->getTitle() ) );
-                $entity->setBody( strip_tags( $entity->getBody() ) );
+                $entity->setBody( htmlentities( $entity->getBody() ) );
 
                 $slug = $entity->getTitle();
                 $entity->setSlug(Util::slugify($slug));
